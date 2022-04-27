25eeac0c603dfde2d7faf6ed860955d54e6b77ab
DaTrederVid <dedemacmactretre@gmail.com>
2022-04-26 21:43:10 -0400

    gameroute working tem

--- data/gameApi.js
+++ data/gameApi.js
@@ -1,3 +1,21 @@
 const mongoCollections = require("../config/mongoCollections");
 const games = mongoCollections.games;
 const { ObjectId } = require("mongodb");
+
+let getAverageRatingAmongFriends = function () {
+    //todo
+};
+
+let getGameFromId = function () {
+    //todo
+};
+
+let getImageFromGameId = function () {
+    //todo
+};
+
+module.exports = {
+    getAverageRatingAmongFriends,
+    getGameFromId,
+    getImageFromGameId,
+};
--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -6,14 +6,15 @@ const { gameApi } = require("../data");
 router.route("/:id").get(async (req, res) => {
     let argId = req.params.id; //todo check validity
     let game = gameApi.getGameFromId(argId);
+    let userId = undefined; //todo get user id from session
     let hobj = {
-        game_name: game.title,
+        game_name: game?.title,
         image: gameApi.getImageFromGameId(argId), //todo not implemented
-        alt: `${game.title}`,
-        description: game.description,
+        alt: `${game?.title}`,
+        description: game?.description,
         f_rating: gameApi.getAverageRatingAmongFriends(argId, userId), //todo not implemented. also should depend on if the user is logged in
-        overall_rating: game.avg_rating,
-        reviews: game.reviews, //todo apply function to grab usernames from ids
+        overall_rating: game?.avg_rating,
+        reviews: game?.reviews, //todo apply function to grab usernames from ids
     };
     res.render("pages/game");
 });
