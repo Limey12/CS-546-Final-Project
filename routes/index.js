dd50a2d4da49355767715448b6fe38e8ea57e62e
Matthew DeStefano <mdestefa@stevens.edu>
2022-04-27 16:34:57 -0400

    Established framework for profiles page in index.js, added routes and handlebars

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@
 const gameRoutes = require("./gameRoutes");
 const rootRoute = require("./rootRoute");
 const catalogRoutes = require("./catalogRoutes");
+const profileRoutes = require("./profileRoutes");
 
 const constructorMethod = (app) => {
     app.use("/", rootRoute);
     app.use("/game", gameRoutes);
     app.use("/GameCatalog", catalogRoutes);
+    app.use("/Profile", profileRoutes);
     app.use("*", (req, res) => {
         res.status(404).json({ error: "Not found" });
     });
---
+++ routes/profileRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { gameApi } = require("../data");
+
+//GET http://localhost:3000/Profile/{id}
+router.route("/:id").get(async (req, res) => {
+    let profileId = req.params.id;
+    // Todo:
+    // 1. Check if profileId is valid
+    // 2. Get info from profileId
+    //  - Get user's name
+    //  - Get user's friends
+    //  - Get user's games
+    //  - Get user's reviews
+    // 3. Render profile page
+    res.render("pages/profile");
+});
+
+module.exports = router;
