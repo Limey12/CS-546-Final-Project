aa0f4c6478112b2f34d546f81a1319e75baeab11
DaTrederVid <dedemacmactretre@gmail.com>
2022-04-26 21:00:03 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 //I pledge my honor that I have abided by the Stevens Honor System
 
 const gameRoutes = require("./gameRoutes");
-const rootRoutes = require("./rootRoutes");
+const rootRoute = require("./rootRoute");
 
 const constructorMethod = (app) => {
-    app.use("/", rootRoutes);
+    app.use("/", rootRoute);
     app.use("/game", gameRoutes);
     app.use("*", (req, res) => {
         res.status(404).json({ error: "Not found" });
