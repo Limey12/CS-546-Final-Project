ff026fb8fe5669b8ce0ff181da59e196ec2ce003
Limey24 <bllmd1@gmail.com>
2022-04-28 17:23:51 -0400

    Ran prittier on several more files

--- data/index.js
+++ data/index.js
@@ -2,7 +2,7 @@ const gameApi = require("./gameApi");
 const games = require("./games");
 const users = require("./users");
 module.exports = {
-    gameApi: gameApi,
-    games: games,
-    users: users
+  gameApi: gameApi,
+  games: games,
+  users: users,
 };
