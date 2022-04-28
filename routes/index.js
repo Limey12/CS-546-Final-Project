70a630033f30f02f48bb8388b57b0c48544e3775
Limey24 <bllmd1@gmail.com>
2022-04-28 17:22:22 -0400

    Added login route

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 //I pledge my honor that I have abided by the Stevens Honor System
 
 const gameRoutes = require("./gameRoutes");
+const loginRoute = require("./loginRoute");
 const rootRoute = require("./rootRoute");
 const catalogRoutes = require("./catalogRoutes");
 const profileRoutes = require("./profileRoutes");
 
 const constructorMethod = (app) => {
-    app.use("/", rootRoute);
-    app.use("/game", gameRoutes);
-    app.use("/GameCatalog", catalogRoutes);
-    app.use("/Profile", profileRoutes);
-    app.use("*", (req, res) => {
-        res.status(404).json({ error: "Not found" });
-    });
+  app.use("/", rootRoute);
+  app.use("/login", loginRoute);
+  app.use("/game", gameRoutes);
+  app.use("/GameCatalog", catalogRoutes);
+  app.use("/Profile", profileRoutes);
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
 };
 
 module.exports = constructorMethod;
