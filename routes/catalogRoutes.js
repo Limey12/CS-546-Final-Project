b005ff6bf68e3bc1f7e3fcbb1478cd917b681973
MatthewVaysfeld <55519323+MattVaysfeld@users.noreply.github.com>
2022-04-30 16:37:25 -0400

    start of search bar

--- routes/catalogRoutes.js
+++ routes/catalogRoutes.js
@@ -5,11 +5,42 @@ const { gameApi,games } = require("../data");
 
 //GET http://localhost:3000/GameCatalog
 router.route("/").get(async (req, res) => {
-    let allGames = await games.getAllGames();
-    if(!allGames){
-        throw "no games in database"
+    try{
+        let allGames = await games.getAllGames();
+        if(!allGames){
+            throw "No games in database"
+        }
+        if(allGames.length == 0){
+            throw "No games in database"
+        }
+        res.status(400).render("pages/catalog", {games:allGames});
+    } catch(e){
+        res.status(400).render("pages/catalog", {error:true,errormsg:e});
     }
-    res.status(404).render("pages/catalog", {games:allGames});
+    
+});
+
+//POST 
+router.post('/searchgames', async (req, res) => {
+    let search = req.body.gameSearchTerm;
+    if (!search) {
+        res.status(400).render('pages/catalog',{title:"Show Finder",gameSearchTerm:search,error:true,errormsg:"No searchterm inputted"});
+        return;
+    }
+    else{
+        search = search.trim();
+        if(!search){
+            res.status(400).render('pages/catalog',{title:"Show Finder",gameSearchTerm:search,error:true,errormsg:"No searchterm inputted"});
+            return;
+        }
+    }
+    
+    games = await gameData.getAllGames(); //todo
+    if(games.length == 0){
+        res.status(400).render('pages/catalog',{title:"Show Finder",gameSearchTerm:search,error:true,errormsg:"No results"});
+        return;
+    }
+    res.render('pages/catalog',{title:"Show Finder",gameSearchTerm:search});
 });
 
 module.exports = router;
