55ed75ab57a1e8bf5da747ace22bb84f11733d25
DaTrederVid <dedemacmactretre@gmail.com>
2022-05-04 21:38:36 -0400

    improve game page

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,22 +1,31 @@
 const express = require("express");
+const constructorMethod = require(".");
 const router = express.Router();
-const { games } = require("../data");
+const { games, users } = require("../data");
 
 //GET http://localhost:3000/game/{id}
 router.route("/:id").get(async (req, res) => {
-    let argId = req.params.id; //todo check validity
-    let game = await games.getGame(argId);
-    let userId = undefined; //todo get user id from session
-    let hobj = {
-        game_name: game?.title,
-        image: games.getImage(argId), //todo not implemented
-        alt: `${game?.title}`,
-        description: game?.description,
-        f_rating: games.getAverageRatingAmongFriends(argId, userId), //todo not implemented. also should depend on if the user is logged in
-        overall_rating: game?.avg_rating,
-        reviews: game?.reviews, //todo apply function to grab usernames from ids
-    };
-    res.render("pages/game", hobj);
+    try {
+        let argId = req?.params?.id;
+        if (argId == undefined || typeof argId != 'string') {
+            //todo error page
+        }
+        let game = await games.getGame(argId);
+        let userId = req?.session?.user?.id;
+
+        let hobj = {
+            game_name: game?.title,
+            image: await games.getImage(argId), //todo not implemented
+            alt: `${game?.title}`,
+            description: game?.description,
+            f_rating: await games.getAverageRatingAmongFriends(argId, userId), //todo not implemented. also should depend on if the user is logged in
+            overall_rating: game?.avg_rating,
+            reviews: game?.reviews, //todo apply function to grab usernames from ids
+        };
+        res.render("pages/game", hobj);
+    } catch (e) {
+        return res.status(400).send(e);
+    }
 });
 
 module.exports = router;
