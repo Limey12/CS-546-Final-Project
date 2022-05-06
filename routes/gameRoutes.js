23c611a4c642115153a91e4d1d682a8eafc89f23
DaTrederVid <dedemacmactretre@gmail.com>
2022-05-06 11:51:38 -0400

    Changed message for unavailable ratings

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -13,13 +13,17 @@ router.route("/:id").get(async (req, res) => {
         let game = await games.getGame(argId);
         let userId = req?.session?.user?.id;
         // console.log("before hobj")
+        // console.log(req.session.user);
+        // console.log(userId);
+        // console.log(argId);
         let hobj = {
+            logged_in: userId != undefined,
             game_name: game?.title,
             image: await games.getImage(argId),
             alt: `${game?.title}`,
             description: game?.description ?? "No description available",
-            f_rating: await games.getAverageRatingAmongFriends(userId, argId) ?? "N/A", //todo not implemented. also should depend on if the user is logged in
-            overall_rating: game?.avg_rating ?? "N/A",
+            f_rating: await games.getAverageRatingAmongFriends(userId, argId) ?? "None of your freinds have rated this game!", //todo not implemented. also should depend on if the user is logged in
+            overall_rating: game?.overallRating ?? "No one has rated this game!",
             reviews: game?.reviews, //todo apply function to grab usernames from ids
         };
         res.render("pages/game", hobj);
