6e83f7a02e5e7dba489ded5a6ed4a4226134f951
DaTrederVid <dedemacmactretre@gmail.com>
2022-05-05 20:30:14 -0400

    add reviews and comments data files

--- data/index.js
+++ data/index.js
@@ -1,6 +1,11 @@
 const games = require("./games");
 const users = require("./users");
+const reviews = require("./reviews");
+const comments = require("./comments");
+
 module.exports = {
   games: games,
   users: users,
+  reviews: reviews,
+  comments: comments
 };
---
+++ data/reviews.js
@@ -0,0 +1,13 @@
+
+let getGameFromReview = async function(reviewID) {
+  //from a reviewID, return a gameID
+}
+
+let getRatingFromReview = async function(reviewID) {
+  //from a reviewID, return the rating
+}
+
+module.exports = {
+    getGameFromReview,
+    getRatingFromReview
+}
