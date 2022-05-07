d9a7803b784d1bb7e88d7349ed27877bafb87b03
MatthewVaysfeld <55519323+MattVaysfeld@users.noreply.github.com>
2022-05-06 23:17:22 -0400

    added client side validation for user search bar

---
+++ public/js/users.js
@@ -0,0 +1,28 @@
+$(function ($) {
+    var form = $("#usersearch");
+    var searchTermInput = $("#userSearchTerm");
+    var error = $("#searcherror");
+  
+    form.submit(function(){
+      try {
+        var searchTerm = searchTermInput.val();
+        if(!searchTerm){
+          throw "Search term needs to be inputted";
+        }
+        if(typeof searchTerm !== "string"){
+          throw "Search term needs to be a string";
+        }
+        searchTerm = searchTerm.trim();
+        if(searchTerm.length == 0){
+          throw "Search term needs to be a non empty string";
+        }
+        error.hide();
+        return true;
+      } catch(e){
+        error.html(e);
+        error.show();
+        return false;
+      }
+  });
+  })(window.jQuery);
+  
