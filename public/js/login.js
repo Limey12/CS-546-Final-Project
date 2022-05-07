8143cc13ccabb54dacdfda14b972fd6914702506
Limey24 <bllmd1@gmail.com>
2022-05-06 22:32:43 -0400

    Added client side validation for password password

--- public/js/login.js
+++ public/js/login.js
@@ -10,11 +10,14 @@ $(function ($) {
     try {
       var username = usernameInput.val(),
         password = passwordInput.val(); // Need this
-      if (!username) throw "Username needs to be inputted";
-      if (typeof username !== "string") throw "Username needs to be a string";
-      username = username.trim();
-      if (username.length < 4) throw "Username must be at least 4 characters long";
+      if (!username || !password) throw "Must provide username and password.";
+      if (typeof username !== "string") throw "Username must be a string";
+      username = username.toLowerCase();
       if (!/^[a-z0-9]+$/i.test(username)) throw "Username must be alphanumeric.";
+      if (username.length < 4) throw "Username must be at least 4 characters long";
+      if (typeof password !== "string") throw "Password must be a string";
+      if (/\s/.test(password)) throw "Password must not contain any spaces.";
+      if (password.length < 6) throw "Password must be at least 6 characters long.";
       error.hide();
       return true;
     } catch (e) {
