185ce144c9c744a3d1e331ac1d3e5777ed957d35
Limey24 <bllmd1@gmail.com>
2022-05-07 17:17:39 -0400

    Fleshed out client side error checking

--- public/js/bio.js
+++ public/js/bio.js
@@ -1,15 +1,19 @@
 $(function ($) {
-    var bioForm = $("#bio-form");
-    var bioInput = $("#newBio");
-    var error = $("error");
-    bioForm.submit(function () {
-      try {
-        error.hide();
-      }catch (e) {
-        error.html(e);
-        error.show();
-        return false;
-      }
-    });
-  })(window.jQuery);
-  
+  var bioForm = $("#bio-form");
+  var bioInput = $("#newBio");
+  var error = $("error");
+  bioForm.submit(function () {
+    try {
+      var bio = bioInput.val();
+      if (!bio) throw "Must provide bio";
+      if (typeof bio !== "string") throw "Bio must be a string";
+      bio = bio.trim();
+      if (bio.length == 0) throw "Bio must be a non empty string";
+      error.hide();
+    } catch (e) {
+      error.html(e);
+      error.show();
+      return false;
+    }
+  });
+})(window.jQuery);
