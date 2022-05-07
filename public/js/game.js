0fe0ca8eab61f124722e9f1b284198a4460d083f
DaTrederVid <dedemacmactretre@gmail.com>
2022-05-06 20:11:00 -0400

    add todo comment

--- public/js/game.js
+++ public/js/game.js
@@ -2,7 +2,9 @@
   window.onload = function () {
     var fav = $("#fav_btn");
     var lfav = $("#least_fav_btn");
-
+    //todo for both functions add to list using ajax.
+    //clear fields.
+    //do more client side error checking
     fav.submit(function (e) {
       e.preventDefault();
       $.ajax({
